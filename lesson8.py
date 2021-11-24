c29bccabc7603241b66b5c7c120c7c6e59d3a465
Almaz <almazbot@gmail.com>
2021-11-25 02:37:36 +0400

    8 урок

---
+++ lesson8.py
@@ -0,0 +1,16 @@
+with open('gitignore.txt', 'w') as l8:
+    l8.writelines(['/venv\n', '.idea'])
+
+with open('gitignore.txt', 'r') as l8:
+    f = l8.readlines()
+    sep = ','
+    q1 = sep.join(f)
+    q1 = q1[::-1]
+    q1 = q1.split(',')
+    l8.seek(0)
+    print(l8.readlines())
+
+with open('lesson8_file.txt', 'r+') as l8_2:
+    l8_2.writelines(q1)
+    l8_2.seek(0)
+    print(l8_2.readlines())
